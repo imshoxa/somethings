6177b87f38eea4dc9608669b5444d53b70e2237d
Shohruh Salimov <shohruhsalimov@Shohruhs-MacBook-Air.local>
2025-12-11 21:55:11 +0500

    express-js from zero

---
+++ express-js/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/user/:username', (req, res) => {
+    let data = {username: req.params.username, hobbies: ['Swimming', 'Football', 'Computer Games']}
+    res.render('user', data)
+})
+app.post('/check-user', (req, res) =>{
+    let username = req.body.username
+    if(username == "")
+        return res.redirect('/')
+    else
+        return res.redirect('/user/' + username)    
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started: http://localhost:${PORT}`)
+})
